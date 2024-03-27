f313b79273dbddccfa0d35f083f0f1e82449343d
KazzK9 <yunus-t3p@hotmail.fr>
2024-03-27 16:28:12 +0100

    Your commit message here

--- app.py
+++ app.py
@@ -7,15 +7,11 @@ import logging
 import os
 import glob
 
-def remove_data(df: pd.DataFrame, last_n_samples: int = 4*3):
-
-    # df: pd.DataFrame = pd.read_csv(fic_export_data)
-    return df.iloc[:-last_n_samples]
-    # df.to_csv(fic_export_data, index=False)
-
 logging.basicConfig(level=logging.INFO)
 
+# Constants
 LAG_N_DAYS: int = 7
+EXPECTED_SAMPLES_PER_DAY: int = 24  # or however many you expect per day
 
 # Initialize directories for data storage
 os.makedirs("data/raw/", exist_ok=True)
@@ -31,25 +27,31 @@ for file_path in glob.glob("data/raw/*json"):
 # Title for your app
 st.title("Data Visualization App")
 
-# Define a function to load daily data
-@st.cache(ttl=15 * 60)  # Corrected decorator to @st.cache
+# Function to load daily data
+@st.cache(ttl=15 * 60)
 def load_data(lag_days: int):
     load_data_from_lag_to_today(lag_days)
     main_process()
     data = pd.read_csv("data/interim/daily_data.csv", parse_dates=[col_date])
     return data
 
-# Load daily data
-df = load_data(LAG_N_DAYS)
-df = remove_data(df, last_n_samples=4*24)
+# Function to remove the last N samples and return the number of removed samples
+def remove_data(df: pd.DataFrame, last_n_samples: int = EXPECTED_SAMPLES_PER_DAY):
+    removed_data = df.tail(last_n_samples)
+    df.drop(df.tail(last_n_samples).index, inplace=True)
+    return df, removed_data.shape[0]
 
-# Creating a line chart for daily data
+# Load and clean daily data
+df, num_removed_samples = remove_data(load_data(LAG_N_DAYS), last_n_samples=EXPECTED_SAMPLES_PER_DAY)
+
+# Displaying the line chart for daily data
 st.subheader("Line Chart of Numerical Data Over Time")
 fig = px.line(df, x=col_date, y=col_donnees, title="Consommation en fonction du temps")
 st.plotly_chart(fig)
 
-# Calculate total consumption for the last 7 days
+# Calculate and display total consumption for the last 7 days
 recent_consumption = df[df[col_date] >= (df[col_date].max() - pd.Timedelta(days=7))][col_donnees].sum()
-
-# Display total consumption for the last 7 days
 st.subheader(f"Total Consumption for the Last 7 Days: {recent_consumption:.2f} units")
+
+# Display the number of removed samples
+st.subheader(f"Number of Missing Data Points: {num_removed_samples}")
